c49f9c2ef0569283163c8fc7f81abd0359f433d9
Pranay Anchuri <panchuri@princeton.edu>
2019-12-20 17:39:20 -0500

    intersection obs

--- WebScience/Studies/content-scripts/linkExposure.js
+++ WebScience/Studies/content-scripts/linkExposure.js
@@ -7,52 +7,49 @@
      */
     let updateInterval = 2000;
     linkExposure();
+
+
     /**
      *main function that looks for exposure to known domains. For known short urls, it uses LinkResolution 
      *utility to get the actual url and checks for the presence of a known domain
     */
   function linkExposure() {
-    // configure the intersection observer instance
-    var intersectionObserverOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.0
-    };
-    var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
-    function onIntersection(entries){
-      entries.forEach(entry => {
-        //entry.target.classList.toggle('visible', entry.intersectionRatio > 0);
-        // Are we in viewport?
-        if (entry.intersectionRatio > 0) {
-          // check if the target is short url
-          if(testForMatch(shortURLMatcher, entry.target.href)) {
-              sendMessageToBackground("WebScience.shortLinks", [{href: entry.target.href}]);
-          }
-          // check for domain matching
-          if(testForMatch(urlMatcher, entry.target.href)) {
-              sendMessageToBackground("WebScience.linkExposureInitial", [{href: entry.target.href, size: getElementSize(entry.target)}]);
-          }
-            entry.target.isVisited = true;
-            // Stop watching the target
-            observer.unobserve(entry.target);
-        }
-      });
+
+    // Save the time the page initially completed loading
+    let initialLoadTime = Date.now();
+    let initialVisibility = document.visibilityState == "visible";
+
+    /**
+     * Convert relative url to abs url
+     * @param {string} url 
+     */
+    function rel_to_abs(url) {
+      /* Only accept commonly trusted protocols:
+       * Only data-image URLs are accepted, Exotic flavours (escaped slash,
+       * html-entitied characters) are not supported to keep the function fast */
+      if (/^(https?|file|ftps?|mailto|javascript|data:image\/[^;]{2,9};):/i.test(url))
+        return url; //Url is already absolute
+
+      var base_url = location.href.match(/^(.+)\/?(?:#.+)?$/)[0] + "/";
+      if (url.substring(0, 2) == "//")
+        return location.protocol + url;
+      else if (url.charAt(0) == "/")
+        return location.protocol + "//" + location.host + url;
+      else if (url.substring(0, 2) == "./")
+        url = "." + url;
+      else if (/^\s*$/.test(url))
+        return ""; //Empty = Return nothing
+      else url = "../" + url;
+
+      url = base_url + url;
+      var i = 0;
+      while (/\/\.\.\//.test(url = url.replace(/[^\/]+\/+\.\.\//g, "")));
+
+      /* Escape certain characters to prevent XSS */
+      url = url.replace(/\.$/, "").replace(/\/\./g, "").replace(/"/g, "%22")
+        .replace(/'/g, "%27").replace(/</g, "%3C").replace(/>/g, "%3E");
+      return url;
     }
-
-  // Save the time the page initially completed loading
-  let initialLoadTime = Date.now();
-  let initialVisibility = document.visibilityState == "visible";
-
-  /**
-   * function to test if a visiable DOM element matches the given reg exp
-   * @param {RegExp} matcher regular expression matcher
-   * @param {string} link url
-   * @param {Element} element DOM element
-   */
-  function testForMatch(matcher, link) {
-    // if element is not null check if its in the viewport
-    return matcher.test(link);
-  }
 
     /**
      * Helper function to get size of element
@@ -61,10 +58,22 @@
     function getElementSize(el) {
       let rect = el.getBoundingClientRect();
       return {
-        width: rect.right - rect.left,
-        height: rect.bottom - rect.top,
-        rect: rect
+        width: rect.width,
+        height: rect.height
       };
+    }
+
+
+    /**
+     * Get link size
+     * 
+     * @param {string} link url on the page
+     */
+    function getLinkSize(link) {
+      // create an object with key = init and value is resolved url
+      let query = "a[href='"+link+"']";
+      let elements = document.body.querySelectorAll(query);
+      return (elements.length > 0 ? getElementSize(elements[0]) : null);
     }
 
     /**
@@ -87,52 +96,53 @@
       }
     }
 
-    /**
-     * Get link size
-     * 
-     * @param {string} link url on the page
-     */
-    function getLinkSize(link) {
-      // create an object with key = init and value is resolved url
-      let query = "a[href='"+link+"']";
-      let elements = document.body.querySelectorAll(query);
-      return (elements.length > 0 ? getElementSize(elements[0]) : null);
+    function listen(element) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.isObserved = true;
+            let url = rel_to_abs(entry.target.href);
+            if (shortURLMatcher.test(url)) {
+              sendMessageToBackground("WebScience.shortLinks", [{ href: url }]);
+            }
+            // check for domain matching
+            if (urlMatcher.test(url)) {
+              sendMessageToBackground("WebScience.linkExposureInitial", [{ href: url, size: getElementSize(entry.target) }]);
+            }
+            observer.unobserve(entry = entry.target);
+          }
+        });
+      });
+      observer.observe(element);
     }
 
+
     /**
-     * Function to look for new <a> elements in the viewport, tests for matches to study domains and short domains.
-     * The matched urls are sent to background script
+     * Function to look for new <a> elements and create an intersection obs listener
      */
-  function matchLinks() {
-    /*
-    Filter for elements that haven't been visited previously and observe them with intersection observer
-    Use isVisited attribute to track elements that have been visited
-    */
-   Array.from(document.body.querySelectorAll("a[href]")).filter(link => link.isVisited == null).forEach(element => {
-     observer.observe(element);
-   });
-   /*(let aElements = Array.from(document.body.querySelectorAll("a[href")).filter(link => link.isVisited == null).map(
-     element => {
-       observer.observe(element);
-       element.isVisited = true;
-       return element;
-     }
-   );*/
-  }
+    function matchLinks() {
+      /*
+      Filter for elements that haven't been visited previously and observe them with intersection observer
+      */
+      Array.from(document.body.querySelectorAll("a[href]")).filter(link => link.isObserved == null).forEach(element => {
+        listen(element);
+      });
+    }
 
   // call update every 2 seconds
   setInterval(matchLinks, updateInterval);
+
     browser.runtime.onMessage.addListener((data, sender) => {
       let dest = data.dest;
       let source = data.source;
-      if(testForMatch(urlMatcher, dest)) {
+      if (testForMatch(urlMatcher, dest)) {
         // get source size
         let sz = getLinkSize(source);
-        let data = [{href: dest, size: sz}];
+        let data = [{ href: dest, size: sz }];
         sendMessageToBackground("WebScience.linkExposureInitial", data);
       }
       return Promise.resolve({ response: "received messages" });
     });
-  }
-}
-)();
+  } // End of link exposure function
+} // end of anon function
+)(); // encapsulate and invoke
