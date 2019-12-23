1c944ca7ebd8ace9eed351b384e5420ed98c0910
Pranay Anchuri <panchuri@princeton.edu>
2019-12-23 12:01:43 -0500

    adding comments and map to set

--- WebScience/Utilities/LinkResolution.js
+++ WebScience/Utilities/LinkResolution.js
@@ -2,26 +2,49 @@ import { getDebuggingLog } from './Debugging.js';
 const debugLog = getDebuggingLog("Studies.LinkExposure");
 
 
+// promiseStore is a mapping from a url to resolve and the resolve objects associated with it
 let promiseStore = new Map();
-let trackLinks = new Map();
+// trackLinks is a set for which the headers are observed
+let trackLinks = new Set();
+// links is a mapping from a redirected url to url fetched
+// recursively traversing this mapping will get the redirect chain associated with an initial url
 let links = new Map();
 
+/**
+ * Function to resolve a given url to the final url that it points to
+ * @param {string} url to resolve
+ * @returns promise
+ */
 export function resolveURL(url) {
   var p = new Promise(function (resolve, reject) {
-    // store this resolve object in the store
+    // store this resolve object in promiseStore
     let resolves = promiseStore[url] || [];
     resolves.push(resolve);
     promiseStore.set(url, resolves);
+    trackLinks.add(url);
     // fetch this url
     fetch(url, { redirect: 'manual', headers: { 'User-Agent': '' } });
   });
   return p;
 }
 
+/**
+ * Function to extract location value from response header.
+ * The response header from onHeadersReceived is an array of objects, one of which possibly
+ * contains a field with name location (case insensitive).
+ * This function finds such an object
+ * 
+ * @param {Array} headers array containing response headers
+ * @returns {name : "location", value: <url>} object containing location value or null
+ */
 function getLocationFromResponseHeader(headers) {
-  return headers.find(obj => {return obj.name == "location"; });
+  return headers.find(obj => {return obj.name.toUpperCase() === "LOCATION"; });
 }
 
+/**
+ * Listener for https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onHeadersReceived
+ * @param {JSON} details contains objects from headers
+ */
 function responseHeaderListener(details) {
   // Continue only if this url is relevant for link resolution
   if(!trackLinks.has(details.url)) {
@@ -35,7 +58,7 @@ function responseHeaderListener(details) {
     // Create a link between the next url and the initial url
     links.set(nexturl, details.url);
     // Add the next url so that we process it during the next onHeadersReceived
-    trackLinks.set(nexturl, true);
+    trackLinks.add(nexturl);
     // Send fetch request to the next url
     fetch(nexturl, { redirect: 'manual', headers: { 'User-Agent': '' } });
   } else { // url is not redirected
@@ -43,7 +66,10 @@ function responseHeaderListener(details) {
       // backtrack links to get to the promise object that corresponds to this
       let url = details.url;
       while (links.has(url)) {
+        let temp = url;
         url = links.get(url);
+        links.delete(temp);
+        trackLinks.delete(temp);
       }
       // url now contains the initial url. Now, resolve the corresponding promises
       if (url && promiseStore.has(url)) {
@@ -59,4 +85,5 @@ function responseHeaderListener(details) {
   }
 }
 
+// register listener for header
 browser.webRequest.onHeadersReceived.addListener(responseHeaderListener, {urls : ["<all_urls>"]}, ["responseHeaders"]);
