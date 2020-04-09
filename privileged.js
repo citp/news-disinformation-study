4446cccef999cee579c799e76f3e372b931f44da
Pranay Anchuri <panchuri@princeton.edu>
2020-04-09 19:20:58 -0400

    consent and survey popup using events from privileged api

--- privileged.js
+++ privileged.js
@@ -14,13 +14,27 @@ var {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
 // load utilityOverlay module for openLinkIn
 Services.scriptloader.loadSubScript("chrome://browser/content/utilityOverlay.js", this);
 
-const DEFAUL_WINDOW_LOCATION = "chrome://browser/content/aboutDialog.xhtml"
+const DEFAULT_WINDOW_LOCATION = "chrome://browser/content/aboutDialog.xhtml"
 
 XPCOMUtils.defineLazyModuleGetter(
     this,
     "BrowserWindowTracker",
     "resource:///modules/BrowserWindowTracker.jsm",
 );
+
+let gManager = {
+    onSurveyPopupListeners: new Set(),
+    onConsentPopupListeners: new Set(),
+  };
+
+  function generateUUID(base) { // Public Domain/MIT
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        r = (base + r)%16 | 0;
+        //d = Math.floor(d/16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 this.privileged = class extends ExtensionAPI {
     getAPI(context) {
@@ -41,27 +55,45 @@ this.privileged = class extends ExtensionAPI {
                         { // main action
                             label: "Learn more",
                             accessKey: "L",
-                            callback: function() { }
+                            callback: function() {
+                                gManager.onConsentPopupListeners.forEach((listener) => {
+                                    listener(0);
+                                  });
+                            }
                         },
                         [ // secondary actions
                             {
-                                label: "No thanks",
-                                accessKey: "N",
-                                callback: function() { }
-                            }
+                                label: "Yes, I agree",
+                                accessKey: "1",
+                                callback: function() {
+                                  //currentWindow.alert("First secondary option selected.");
+                                gManager.onConsentPopupListeners.forEach((listener) => {
+                                    listener(1);
+                                  });
+                                }
+                              },
+                              {
+                                label: "No, I disagree",
+                                accessKey: "2",
+                                callback: function() {
+                                  //currentWindow.alert("Second secondary option selected.");
+                                gManager.onConsentPopupListeners.forEach((listener) => {
+                                    listener(-1);
+                                  });
+                                }
+                              }
                         ],
                         // temporary options
                         {
                             "persistence": 10,
                             "persistWhileVisible": true,
                             "dismissed": false,
-                            // if we go with something like this, we'll need to use the right (light or dark) version
-                            //"popupIconURL": //couldn't get a relative path to work, used absolute for screenshots "file:///pathtohere/icons/princeton_university_shield_light.png"
+                            "popupIconURL": "chrome://browser/content/logos/tracking-protection.svg"
                         }
                     );
 
                 },
-                createSurveyPopup(url) {
+                createSurveyPopup(url, surveyTime) {
                     var currentWindow = BrowserWindowTracker.getTopWindow({
                         private: false,
                         allowPopups: false,
@@ -77,16 +109,31 @@ this.privileged = class extends ExtensionAPI {
                             label: "Open survey",
                             accessKey: "O",
                             callback: function () {
-                                var features = "chrome,";
-                                features += "centerscreen,dependent,resizeable,location=1,scrollbars=1,status=1";
-                                //privilegedEventEmitter.emitSurveyConsentAccept();
-                                let window = currentWindow.open(DEFAUL_WINDOW_LOCATION, "", features);
-                                window.location.assign(url);
-                                //let newlocation2 = "https://google.com";
-                                //currentWindow.alert(ww.openWindow);
+                                let surveyId = generateUUID(surveyTime);
+                                gManager.onSurveyPopupListeners.forEach((listener) => {
+                                    listener(url+surveyId);
+                                  });
+                                /*
+                                Methods which didn't work :
+                                1. open a window with default location and
+                                   assign new url
+                                   --> This works only for pages in
+                                   browser/content
+                                //var features = "chrome,";
+                                //features += "centerscreen,dependent,resizeable,location=1,scrollbars=1,status=1";
+                                //let window = currentWindow.open(DEFAULT_WINDOW_LOCATION, "", features);
+                                //window.location.assign(url);
+                                2. Use window watcher component, open a new windom
                                 //var win = ww.openWindow(null, "chrome://browser/content/aboutDialog.xhtml",
-                                //"aboutMyExtension", "chrome,centerscreen", null);
-                                //currentWindow.gBrowser.selectedBrowser.loadOneTab(url, {inBackground: false});
+                                   //"aboutMyExtension", "chrome,centerscreen",
+                                   null);
+                                3. use methods on selected browsers
+                                   //currentWindow.gBrowser.selectedBrowser.loadOneTab(url,
+                                   {inBackground: false});
+                                4. Send browser/browser.tabs function as an
+                                   argument from the background script. These
+                                   objects cannot be pickled.
+                                */
                              }
                         },
                         [ // secondary actions
@@ -102,12 +149,37 @@ this.privileged = class extends ExtensionAPI {
                             "persistence": 10,
                             "persistWhileVisible": true,
                             "dismissed": false,
-                            // if we go with something like this, we'll need to use the right (light or dark) version
-                            "popupIconURL": "chrome://popupnotifications/skin/mozlogo.png"
+                            "popupIconURL": "chrome://browser/content/logos/tracking-protection.svg"
                         }
                     );
 
-                }
+                },
+                onSurveyPopup: new ExtensionCommon.EventManager({
+                    context: context,
+                    name: "privileged.onSurveyPopup",
+                    register: (fire) => {
+                      let listener = (id, data) => {
+                        fire.async(id, data);
+                      };
+                      gManager.onSurveyPopupListeners.add(listener);
+                      return () => {
+                        gManager.onSurveyPopupListeners.delete(listener);
+                      };
+                    }
+                  }).api(),
+                onConsentPopup: new ExtensionCommon.EventManager({
+                    context: context,
+                    name: "privileged.onConsentPopup",
+                    register: (fire) => {
+                      let listener = (id, data) => {
+                        fire.async(id, data);
+                      };
+                      gManager.onConsentPopupListeners.add(listener);
+                      return () => {
+                        gManager.onConsentPopupListeners.delete(listener);
+                      };
+                    }
+                  }).api()
             }
         }
     }
