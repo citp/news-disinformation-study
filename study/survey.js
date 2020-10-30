0e91f13922cefb1380e415491c633ebc2e00dee5
Anne Kohlbrenner <akohlbre@andrew.cmu.edu>
2020-10-30 01:14:42 -0400

    text for survey request popup

--- study/survey.js
+++ study/survey.js
@@ -2,8 +2,11 @@ function listenForClicks() {
     document.addEventListener("click", async (e) => {
         if (e.target.name == "agree") {
             await browser.runtime.sendMessage({ type: "WebScience.Utilities.UserSurvey.openSurveyTab" });
+        } else if (e.target.name == "later") {
+            window.close();
         } else if (e.target.name == "never") {
             await browser.runtime.sendMessage({ type: "WebScience.Utilities.UserSurvey.cancelSurveyRequest" });
+            window.close();
         } else if (e.target.id == "notice") {
             await browser.runtime.sendMessage({ type: "WebScience.Utilities.Consent.openNotice" });
         }
