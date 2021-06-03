1c80e95ac941529f2bb11386ddda18a7aecb3767
Anne Kohlbrenner <akohlbre@andrew.cmu.edu>
2021-06-02 22:17:04 -0400

    Use web-ext config from template study.
    The `browserConsole` option causes a flood of errors whenever the
    extension code tries to access the console (e.g. `console.log`), and I
    couldn't find documentation for it, so I removed it. The appropriate
    console for the extension still opens.

--- web-ext-config.js
+++ web-ext-config.js
@@ -2,35 +2,51 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- module.exports = {
-    // Global options:
-    verbose: true,
-    // Command options:
-    build: {
-      overwriteDest: true,
-    },
-    run: {
-      firefox: "nightly",
-      pref: ["extensions.experiments.enabled=true"],
-    },
-    ignoreFiles: [
-      "bin",
-      "docs",
-      "scripts",
-      "src",
-      "stories",
-      "support",
-      "tests",
-      "CHANGELOG.md",
-      "CODE_OF_CONDUCT.md",
-      "copyright.txt",
-      "LICENSE",
-      "package-lock.json",
-      "package.json",
-      "README.md",
-      "rollup.config.*",
-      "web-ext-config.js",
-      "public/**/*.map",
-    ],
-  };
-  
+// This is the web-ext configuration for the study template. It is
+// part of the build system, and you should not have to modify it.
+
+// Try to read the extension ID from the WebExtensions manifest.
+let extensionID = null;
+try {
+  const fs = require("fs");
+  const manifestJSON = fs.readFileSync("./manifest.json");
+  const manifestObj = JSON.parse(manifestJSON);
+  extensionID = manifestObj.browser_specific_settings.gecko.id;
+}
+catch(error) {
+  // If this block is empty, there is a linter error.
+  extensionID = null;
+}
+
+module.exports = {
+  // Global options:
+  verbose: true,
+  // Command options:
+  build: {
+    overwriteDest: true,
+  },
+  run: {
+    startUrl: [
+      extensionID !== null ? `about:devtools-toolbox?id=${extensionID}&type=extension`: "about:debugging#/runtime/this-firefox"
+    ]
+  },
+  ignoreFiles: [
+    "bin",
+    "docs",
+    "scripts",
+    "src",
+    "stories",
+    "support",
+    "tests",
+    "CHANGELOG.md",
+    "CODE_OF_CONDUCT.md",
+    "copyright.txt",
+    "LICENSE",
+    "package-lock.json",
+    "package.json",
+    "README.md",
+    "rollup.config.*",
+    "web-ext-config.js",
+    "public/**/*.map",
+  ],
+};
